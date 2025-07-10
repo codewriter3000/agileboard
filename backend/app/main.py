2293143538474fa9cc2e176af80d73a21655f142
Alex Micharski <amicharski@outlook.com>
2025-07-10 14:26:05 -0400

    remove deprecated @app.on_event

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,22 @@
 # app/main.py
 from fastapi import FastAPI
-from app.api import user
+from fastapi.concurrency import asynccontextmanager
+from app.api import user, task
 from app.db.base import init_db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Application startup and shutdown events."""
+    # Initialize the database
+    init_db()
+    yield
+    # Here you could add any cleanup code if needed
 
 app = FastAPI(
     title="AgileBoard API",
-    version="0.1.0"
+    version="0.1.0",
+    lifespan=lifespan
 )
 
 app.include_router(user.router)
-
-@app.on_event("startup")
-def on_startup():
-    init_db()
+app.include_router(task.router)
